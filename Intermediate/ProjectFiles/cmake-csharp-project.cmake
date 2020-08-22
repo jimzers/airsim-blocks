9b1d4de75ea4e3a03510e3845035b526abbdfaf7
jimzers <adamleesaintfrancis@gmail.com>
2020-08-21 22:05:58 -0700

    initial commit

---
+++ Intermediate/ProjectFiles/cmake-csharp-project.cmake
@@ -0,0 +1,5 @@
+set(PROJECT_CSHARP_FILES 
+	"/home/jimzers/Documents/airsim/Unreal/Environments/Blocks 4.25/Plugins/AirSim/Source/AirSim.Build.cs"
+	"/home/jimzers/Documents/airsim/Unreal/Environments/Blocks 4.25/Source/Blocks/Blocks.Build.cs"
+ )
+
